4179149419eb684580f90e648a82a33abe0c148d
Kartikey Kushwaha <kartikey92@gmail.com>
2016-11-01 23:11:42 +0530

    Added toolbar.

--- app/src/main/java/io/github/krtkush/marsexplorer/RoverExplorer/AboutRover/AboutRoverActivity.java
+++ app/src/main/java/io/github/krtkush/marsexplorer/RoverExplorer/AboutRover/AboutRoverActivity.java
@@ -2,16 +2,33 @@ package io.github.krtkush.marsexplorer.RoverExplorer.AboutRover;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.krtkush.marsexplorer.R;
+import timber.log.Timber;
 
 public class AboutRoverActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    private AboutRoverPresenterInteractor presenterInteractor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_rover);
+
+        // Initialise butterknife, timber and the presenter layer
+        ButterKnife.bind(AboutRoverActivity.this);
+        Timber.tag(AboutRoverActivity.this.getClass().getSimpleName());
+        presenterInteractor = new AboutRoverPresenterLayer(this);
+
+        // Setup the toolbar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /**
